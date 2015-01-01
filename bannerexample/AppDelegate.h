a4d2d791cdeb5810876984d009822b9d9d2fa216
Medma macmini <medmamacmini@medma.local>
2015-01-01 15:44:13 +0530

    Initial Commit

---
+++ bannerexample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bannerexample
+//
+//  Created by Medma macmini on 01/01/15.
+//  Copyright (c) 2015 medma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bannerexample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bannerexample
+//
+//  Created by Medma macmini on 01/01/15.
+//  Copyright (c) 2015 medma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
